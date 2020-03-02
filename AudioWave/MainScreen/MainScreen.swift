80fa3fc5b00937eb42a5669ea173757db370e759
aaronium112 <aaron@sphericalwave.com>
2020-03-02 16:31:00 -0400

    dual audio streams and players are werking.

--- AudioWave/MainScreen/MainScreen.swift
+++ AudioWave/MainScreen/MainScreen.swift
@@ -28,6 +28,7 @@ class MainScreen: UIViewController
         
         let audioLibrary = AudioLibrary()
         
+        //FIXME: Refactor into Buttons
         let musicPlaylists = audioLibrary.playlists()
         guard let note = UIImage(systemName: "music.note.list") else { fatalError() }
         let musicPlaylistsScreenData = MusicPlaylistsScreenData(musicPlaylists: musicPlaylists, musicPlayer: musicPlayer)
@@ -35,10 +36,10 @@ class MainScreen: UIViewController
         let musicPlaylistsButton = SwModalButton(image: note, destinationScreen: musicPlaylistsScreen, parentScreen: self)
         navigationItem.leftBarButtonItem = musicPlaylistsButton
         
-        guard let contentPlaylists = audioLibrary.audioBooks() else { fatalError() } //FIXME: Fragile
+        //FIXME: Refactor into Buttons
         guard let book = UIImage(systemName: "book") else { fatalError() }
-        let contentPlaylistsScreenData = ContentPlaylistsScreenData(contentPlaylists: contentPlaylists, contentPlayer: contentPlayer)
-        let contentPlaylistsScreen = ContentPlaylistsScreen(data: contentPlaylistsScreenData)
+        let contentPlaylistsScreenData = MusicPlaylistsScreenData(musicPlaylists: musicPlaylists, musicPlayer: contentPlayer)
+        let contentPlaylistsScreen = MusicPlaylistsScreen(data: contentPlaylistsScreenData, audioPlayer: contentPlayer)
         let contentPlaylistsButton = SwModalButton(image: book, destinationScreen: contentPlaylistsScreen, parentScreen: self)
         navigationItem.rightBarButtonItem = contentPlaylistsButton
     }
