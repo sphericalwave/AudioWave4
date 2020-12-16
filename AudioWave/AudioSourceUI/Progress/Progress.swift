76f14489ebc0c938131c1370532aebb0cb5e5a5a
aaronium112 <aaron@sphericalwave.com>
2020-12-16 12:42:17 -0400

    Revert "long shot to Rob and Jodie. looks solid. vu_meter inbound." Better to use a feature branch for development.
    This reverts commit 037624a8bf8ac6b1f4eea06b938311bda120ac39.

--- AudioWave/AudioSourceUI/Progress/Progress.swift
+++ AudioWave/AudioSourceUI/Progress/Progress.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProgressUI: UIViewController
+class Progress: UIViewController
 {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var elapsedTimeLabel: UILabel!
@@ -16,33 +16,37 @@ class ProgressUI: UIViewController
     var timer: Timer?   //FIXME: Nil & Hidden Dependency
     let audioSource: AudioSource //FIXME: Naming
     let notifications = NotificationCenter.default  //FIXME: Hidden Dependency
-    var displayLink: CADisplayLink? //FIXME: Nil & Hidden Dependency, FIXME: Remove from Run Loop, FIXME: Disable offscreen
-
+    
     init(audioSource: AudioSource) {
         self.audioSource = audioSource
-        super.init(nibName: "ProgressUI", bundle: nil)
+        super.init(nibName: "Progress", bundle: nil)
         notifications.addObserver(self, selector: #selector(playerDidLoad), name: .didLoad, object: audioSource)
         notifications.addObserver(self, selector: #selector(playerDidPlay), name: .didPlay, object: audioSource)
         notifications.addObserver(self, selector: #selector(playerDidPause), name: .didPause, object: audioSource)
-        displayLink = CADisplayLink(target: self, selector: #selector(refresh))
-        displayLink?.add(to: .current, forMode: .default)
-        displayLink?.isPaused = true
     }
     required init?(coder: NSCoder) { fatalError() }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //slider.setThumbImage(#imageLiteral(resourceName: "ProgressSliderThumb"), for: .normal)
+        slider.setThumbImage(#imageLiteral(resourceName: "ProgressSliderThumb"), for: .normal)
     }
     
-    @IBAction func sliderChanged(_ sender: AnyObject) { audioSource.seekTo(percentage: slider.value) }
     @objc func playerDidLoad(notification: Notification) { slider.value = 0 }
-    @objc func playerDidPlay(notification: Notification) { displayLink?.isPaused = false }
-    @objc func playerDidPause(notification: Notification) { displayLink?.isPaused = true }
-    @objc func refresh() {
+    
+    @objc func playerDidPlay(notification: Notification) { startTimer() }
+    
+    @objc func playerDidPause(notification: Notification) { timer?.invalidate() }
+    
+    func startTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { _ in
             self.elapsedTimeLabel.text = self.audioSource.elapsedTime()
             self.remainingTimeLabel.text = self.audioSource.remainingTime()
             self.slider.value = self.audioSource.percentage()
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        audioSource.seekTo(percentage: slider.value)
     }
 }
 
